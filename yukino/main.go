e3d2138225a96829d6c11d0e4babd712b65bac30
hangtiancheng.htc <hangtiancheng.htc@taobao.com>
2026-09-22 10:48:13 +0800

    feat: Introduce new features [skip ci]

---
+++ yukino/main.go
@@ -0,0 +1,22 @@
+// yukino-code-rpc-linux-x64
+// yukino-code-rpc-linux-arm64
+// yukino-code-rpc-darwin-x64
+// yukino-code-rpc-darwin-arm64
+// yukino-code-rpc-win32-x64
+// yukino-code-rpc-win32-arm64
+
+// yukino-code-stdio-linux-x64
+// yukino-code-stdio-linux-arm64
+// yukino-code-stdio-darwin-x64
+// yukino-code-stdio-darwin-arm64
+// yukino-code-stdio-win32-x64
+// yukino-code-stdio-win32-arm64
+
+// yukino-code-ws-linux-x64
+// yukino-code-ws-linux-arm64
+// yukino-code-ws-darwin-x64
+// yukino-code-ws-darwin-arm64
+// yukino-code-ws-win32-x64
+// yukino-code-ws-win32-arm64
+
+package main
